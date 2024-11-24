375d62be614423f3515f975ec2b0398cf34bf545
Naitik <naitik12gupta05@gmail.com>
2024-11-24 11:47:28 +0530

    added static files and requirements.txt

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # Agora Route
     path('', include('agora.urls')),
     path('admin/', admin.site.urls),
 ]
