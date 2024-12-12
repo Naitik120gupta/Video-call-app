21ce0b5bde5cc772a20eb6328036a1a864a9cebd
Naitik <naitik12gupta05@gmail.com>
2024-12-13 01:00:11 +0530

    changed chat using websockets

--- agora/routing.py
+++ agora/routing.py
@@ -1,7 +1,6 @@
 from django.urls import re_path
 from agora.consumers import ChatConsumer
 
-
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room>\w+)/$', ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<room>[\w-]+)/$', ChatConsumer.as_asgi()),
 ]
